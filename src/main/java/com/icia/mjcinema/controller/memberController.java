3dd780f5bc4bfb611041563289fa5ea62f8ee939
lmy9292 <82364289+lmy9292@users.noreply.github.com>
2021-07-05 20:34:49 +0900

    spring-validation 의존성 추가

--- src/main/java/com/icia/mjcinema/controller/memberController.java
+++ src/main/java/com/icia/mjcinema/controller/memberController.java
@@ -3,29 +3,31 @@ package com.icia.mjcinema.controller;
 import java.io.IOException;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.icia.mjcinema.domain.Member;
 import com.icia.mjcinema.dto.JoinMemberForm;
+import com.icia.mjcinema.dto.LoginForm;
 import com.icia.mjcinema.service.memberService;
 
 @Controller
 public class memberController {
 	
+	private static final Logger log = LoggerFactory.getLogger(memberController.class);
+	
 	@Autowired
 	private memberService memberservice;
-	
-	private ModelAndView mav;
-	
-	@Autowired
-	private HttpSession session;
 	
 	@RequestMapping (value="/Members/Login")
 	public String login() {
@@ -38,15 +40,29 @@ public class memberController {
 	}
 	
 	@RequestMapping (value="/Members/memberJoin")
-	public ModelAndView memberjoin(@ModelAttribute JoinMemberForm member) throws IllegalStateException, IOException {
-		mav = memberservice.memberjoin(member);
-		return mav;
+	public String memberjoin(@ModelAttribute @Valid JoinMemberForm joinMemberForm, HttpSession session, BindingResult result) throws IllegalStateException, IOException {
+		if (result.hasErrors()) {
+			return "Members/MemberJoinForm";
+		}
+		Member member = memberservice.memberjoin(joinMemberForm);
+		session.setAttribute("loginMember", member);
+		return "redirect:/";
 	}
 	
 	@RequestMapping (value="/Members/memberLogin")
-	public ModelAndView memberlogin (@ModelAttribute Member member) {
-		mav = memberservice.memberlogin(member);
-		return mav;
+	public String memberlogin (@ModelAttribute LoginForm loginForm, HttpSession session, BindingResult result) {
+		Member member;
+		
+		try {
+			member = memberservice.memberlogin(loginForm);
+		} catch (RuntimeException e){
+			FieldError fieldError = new FieldError("loginForm", "invalidIdOrPassword", e.getMessage());
+			result.addError(fieldError);
+			return "Members/Login";
+		}
+		
+		session.setAttribute("loginMember", member);
+		return "redirect:/";
 	}
 	
 	@RequestMapping (value="/Members/memberView")
@@ -55,7 +71,7 @@ public class memberController {
 	}
 		
 	@RequestMapping (value="/Members/memberLogout")
-	public String logout() {
+	public String logout(HttpSession session) {
 		session.invalidate();
 		return "MovieMain";
 	}
@@ -65,10 +81,6 @@ public class memberController {
 		String result = memberservice.idCheck(mid);
 		return result;
 	}
-	
-	@RequestMapping (value="/Movies/MovieReservation")
-	public String moviereservation () {
-		return "/Movies/MovieReservation";
-	}
+
 	
 }
