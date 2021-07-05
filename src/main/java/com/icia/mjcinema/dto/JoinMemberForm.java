71e037ca2b686031714418087508b7ca7f35a662
lmy9292 <82364289+lmy9292@users.noreply.github.com>
2021-07-05 20:35:36 +0900

    유효성 검증 로직추가

--- src/main/java/com/icia/mjcinema/dto/JoinMemberForm.java
+++ src/main/java/com/icia/mjcinema/dto/JoinMemberForm.java
@@ -1,11 +1,15 @@
 package com.icia.mjcinema.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
 
 @Data
 public class JoinMemberForm {
+	
+	@NotEmpty(message="id를 입력해주세요")
 	private String mid;
 	private String mname;
 	private String mpw;
