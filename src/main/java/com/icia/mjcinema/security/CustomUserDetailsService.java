280e87789c5a67feb507637e743fb8b470dbd4ad
tnuren <82364236+tnuren@users.noreply.github.com>
2021-07-19 16:16:40 +0900

    [#42] CustomUserDetailsService 구현

---
+++ src/main/java/com/icia/mjcinema/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.icia.mjcinema.security;
+
+import com.icia.mjcinema.domain.User;
+import com.icia.mjcinema.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Not Found" + username);
+        }
+        return new CustomerUserDetails(user);
+    }
+}
+
